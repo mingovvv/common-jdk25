6c097d36abc392f183010c1eb6d972991acbd84e
mk.jang <mk.jang@twolinecode.com>
2025-11-19 18:34:26 +0900

    init

---
+++ src/main/java/mingovvv/common/CommonJdk25Application.java
@@ -0,0 +1,13 @@
+package mingovvv.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonJdk25Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(CommonJdk25Application.class, args);
+    }
+
+}
---
+++ src/main/java/mingovvv/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package mingovvv.common.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+
+}
